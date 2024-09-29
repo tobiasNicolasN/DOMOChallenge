f2f48c158c6be0ab3eea0e12daf3ca19a54c8799
Tobias Nuñez <tobias.nicolas001@gmail.com>
2024-09-29 15:34:53 -0300

    Disabled button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import style from "../styles/Form.module.css";
 import {useForm, SubmitHandler} from "react-hook-form"
 
@@ -12,8 +12,10 @@ interface IFormData {
 
 function Form() {
   const {register, handleSubmit} = useForm<IFormData>()
+  const [sending, setSending] = useState<boolean>(false)
 
   const onSubmit: SubmitHandler<IFormData> = (data) => {
+    setSending(true)
     console.log(`Nombre: ${data.name}, email: ${data.email}, message: ${data.message}`)
   }
 
@@ -23,10 +25,10 @@ function Form() {
         <button className={style.button}>Book your free consultation</button>
         <h2 className={style.title}>Get a free consultation</h2>
         <div className={style.inputs}>
-          <input className={style.input} {...register("name",{required: true})} placeholder="Name" type="text" />
-          <input className={style.input} {...register("email",{required: true})} placeholder="contact@email.com" type="text" />
+          <input className={style.input} disabled={sending} {...register("name",{required: true})} placeholder="Name" type="text" />
+          <input className={style.input} disabled={sending} {...register("email",{required: true})} placeholder="contact@email.com" type="text" />
         </div>
-        <textarea className={style.textArea} {...register("message",{required: true})} placeholder="Please type your message here..."></textarea>
+        <textarea className={style.textArea} disabled={sending}  {...register("message",{required: true})} placeholder="Please type your message here..."></textarea>
         <button className={style.submitButton}>Get in Touch</button>
       </form>
     </div>
